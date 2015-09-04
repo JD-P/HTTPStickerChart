3916eec0f554ff1abe2145bc1899937771bd2a0b
John David Pressman <jdpostage@linuxmail.org>
2015-09-03 18:16:32 -0700

    Successfully writes *blank* file to output. Further investigation required to fix problem, assumed to be arising from handling of template data.

--- chartboard.py
+++ chartboard.py
@@ -6,7 +6,7 @@ import json
 
 
 
-class AppMain():
+class application():
     """Main script for HTTPStickerChart. Displays the default sticker chart for
     a given user on login."""
     def __init__(self, environ, start_response):
@@ -14,28 +14,54 @@ class AppMain():
         self.start = start_response
 
     def __iter__(self):
+        """Parse query string and take an action based on the action parameter
+        of the string.
+
+        Each method which can be taken as an action is prepended with the prefix
+        do_ and the name of the action appended to it. For example if the query 
+        string is ?action=default then the method do_default would be executed.
+        """
+        query_string = self.environ["QUERY_STRING"]
+        if query_string == '':
+            self.do_default(self.environ)
+        else:
+            query_dict = self.parse_query_string(query_string)
+            try:
+                action = query_dict["ACTION"]
+            except KeyError:
+                return self.no_action_specified()
+            html_gen = getattr(self, "do_" + action)
+            page_html = html_gen(self.environ)
+        status = '200 OK'
+        response_headers = [('Content-type', 'text/plain')]
+        self.start(status, response_headers)
+        return page_html
+
+    def do_default(self, environ):
         # Access current sticker charts
         parent_directory = self.ascend_directory(__file__, 2)
         charts_directory = os.path.join(parent_directory, "charts")
-        charts = os.listdir(charts_directory)
         current_chart = time.strftime("%Y-%m")
         chartpath = os.path.join(charts_directory, current_chart)
         try:
             chart_file = open(chartpath)
         except IOError:
-            self.stock_charts_from_templates(chartpath)
+            #os.makedirs(os.path.split(chartpath)[0], exist_ok=True)
+            with open(chartpath, "w") as chart_file:
+                charts = self.stock_charts_from_templates(chartpath)
+                json_charts = json.dumps(charts)
+                chart_file.write(json_charts)
+                chart_file.flush()
             chart_file = open(chartpath)
-        chart = json.load(chart_file)
-        status = '200 OK'
-        response_headers = [('Content-type', 'text/plain')]
-        self.start(status, response_headers)
-        yield HELLO_WORLD
-
+        charts = json.load(chart_file)
+        chart = charts[0]
+        x_y_table = self.x_to_y(chart["table"])
+        return self.format_table(x_y_table)
 
     def stock_charts_from_templates(self, chartpath):
         """Create sticker chart(s) for the current year and month from the 
         templates given in /charts/templates."""
-        chart_file = open(chartpath, "w")
+        charts = []
         current_time = datetime.datetime.now()
         days_in_month = calendar.monthrange(current_time.year, 
                                             current_time.month)[1]
@@ -43,12 +69,77 @@ class AppMain():
         with open(templatepath) as templates_file:
             templates = json.load(templates_file)
         for template in templates:
+            chart = {}
+            chart["name"] = template["name"]
+            chart["table"] = []
+            for column_name in template["columns"]:
+                column = self.create_column(column_name, days_in_month)
+                chart["table"].append(column)
+            charts.append(chart)
+        return charts
+
+    def create_column(self, column_name, days_in_month):
+        """Create a column given the name of the column and the days in the month
+        for the chart in which che column goes."""
+        column = []
+        column[0] = column_name
+        for day in days_in_month:
+            column.append(None)
+        return column
+
+    def format_table(self, y_x_table):
+        """Format a table whose first coordinate is the Y axis into HTML for 
+        page display."""
+        table_head = "<table>\n"
+        for row in y_x_table:
+            for mark in enumerate(row):
+                if mark[1]:
+                    row[mark[0]] = '\u263A'
+                elif mark[1] is False:
+                    row[mark[0]] = '\\'
+                else:
+                    row[mark[0]] = ' '
+        table_data = "".join(
+            ["<tr>" + "".join(["<td>" + str(mark) + "</td>" for mark in row]) + "</tr>\n" 
+             for row in y_x_table])
+        table_tail = "</table>\n"
+        return table_head + table_data + table_tail
+
+    def x_to_y(self, table):
+        """Convert a two dimensional array whose first coordinate is the X axis
+        to one whose first coordinate is the Y axis."""
+        days_in_month = len(table[0])
+        return [[column[day] for column in table] for day in days_in_month]
             
-        
+
+    def no_action_specified(self):
+        """Error method called to return a web page when no action is given
+        in the query string."""
+        page_html = """<html> 
+                       <head> 
+                       <meta charset='utf-8'> 
+                       <title> Error: No Action Given </title>
+                       </head>
+                       <body>
+                       <p> ERROR: No action was given in the query string. </p>
+                       </body>
+                       </html>"""
+        return page_html
+
+    def parse_query_string(self, query_string):
+        query_dict = {}
+        tokens = query_string.split("&")
+        for token in tokens:
+            tsplit = token.split("=")
+            variable = tsplit[0].upper()
+            value = tsplit[1]
+            query_dict[variable] = value
+        return query_dict
 
     def ascend_directory(self, filepath, steps):
         """Return a filepath which would correspond to ascending n <steps> of 
         <filepath>."""
         while steps:
             filepath = os.path.split(filepath)[0]
+            steps -= 1
         return filepath
