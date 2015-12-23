e0e888227dcc99b9029afec4d06ec5b64185913a
John David Pressman <jdpostage@linuxmail.org>
2015-12-23 02:24:26 -0800

    Changed functions so they return instead of logging to console.

--- js/chart.js
+++ js/chart.js
@@ -1,3 +1,6 @@
+function bootstrap() {
+}
+
 function clicked(row, column) {
    switch (document.getElementsByTagName("table")[0].rows[row].cells[column].getAttribute("bgcolor")) {
     case "white":
@@ -28,15 +31,31 @@ function getTest() {
 	console.error(xhr.statusText);
     }; 
     xhr.send(null);}
-  
-  
+    
+function getDefaultChartname() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "cgi/chartboard.py?action=get_default_chartname", true);
+    xhr.onload = function (e) {
+	if (xhr.readyState === 4) {
+	    if (xhr.status === 200) {
+		return xhr.responseText;
+            } else {
+		console.error(xhr.statusText);
+            }
+	}
+    };
+    xhr.onerror = function (e) {
+	console.error(xhr.statusText);
+    }; 
+    xhr.send(null);}
+
 function createChart(chartname) {
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "cgi/chartboard.py?action=create_chart", true);
     xhr.onload = function (e) {
 	if (xhr.readyState === 4) {
 	    if (xhr.status === 200) {
-		console.log(xhr.responseText);
+		return xhr.responseText;
             } else {
 		console.error(xhr.statusText);
             }
@@ -53,7 +72,7 @@ function createColumn(chartname, columnname) {
     xhr.onload = function (e) {
 	if (xhr.readyState === 4) {
 	    if (xhr.status === 200) {
-		console.log(xhr.responseText);
+		return xhr.responseText;
             } else {
 		console.error(xhr.statusText);
             }
